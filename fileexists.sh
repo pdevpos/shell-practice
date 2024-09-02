5d7a3943f9ca52ef080666c17079e5fcce742aac
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-02 12:12:13 +0530

    read files line by line

--- fileexists.sh
+++ fileexists.sh
@@ -1,5 +1,5 @@
 read -p "Enter directory name:" dir
- source_dir=/home/ec2-user/shell-practice/example
+source_dir=/home/ec2-user/shell-practice/example
 if [ -d $dir ]
 then
   echo "$dir directory exists"
@@ -8,7 +8,7 @@ else
   exit 1
 fi
 FILES=$(find $source_dir -name "*.sh")
-echo "files:::"$FILES
+#echo "files:::"$FILES
 while read -r line; do
-    echo "read line by line : $line"
+  echo "read line by line : $line"
 done < "print files $FILES"
