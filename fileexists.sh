64b2972e9196dd086e2d74b7ed23f1c516b324b5
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-02 11:41:21 +0530

    to check a directory and  file exists or not

--- fileexists.sh
+++ fileexists.sh
@@ -4,14 +4,8 @@ read -p "Enter directory name:" dir
 if [ -d $dir ]
 then
   echo "$dir directory exists"
-  if [ -f $FILE ]
-  then
-    echo "file exists"
-  else
-    echo "file doesn't exists"
-    exit 1
-  fi
 else
   echo "$dir directory doesn't exists"
   exit 1
 fi
+echo "files:::"$FILE
