75d8c62d3cb804417cba33d9c5af4cbec741ed9e
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-02 12:06:56 +0530

    to check a directory and  file exists or not

--- fileexists.sh
+++ fileexists.sh
@@ -1,19 +1,15 @@
 read -p "Enter directory name:" dir
  read -p "Enter filename:" file
  source_dir=/home/ec2-user/shell-practice/example
- FILES=$(find $source_dir -name "*.sh")
 if [ -d $dir ]
 then
   echo "$dir directory exists"
-  if [ -f $FILES ]
-  then
-    echo "$FILES exists"
-  else
-    echo "$FILES does not exists"
-    exit 1
-  fi
 else
   echo "$dir directory doesn't exists"
   exit 1
 fi
-echo "files:::"$FILE
+FILES=$(find $source_dir -name "*.sh")
+echo "files:::"$FILES
+while read -r line; do
+    echo "$line"
+done < "$FILES"
