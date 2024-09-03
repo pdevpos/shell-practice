899ed252c52445b44a2cad597a0812466a9830f3
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-03 14:42:51 +0530

    print vars less than 2

--- usage.sh
+++ usage.sh
@@ -2,7 +2,11 @@ usage()
 {
   echo "usage:: sh backend.sh one.sh two.sh"
 }
+#if [ $# -lt 2 ]; then
+#    echo "call usage"
+#    usage
+#fi
 if [ $# -lt 2 ]; then
-    echo "call usage"
-    usage
+    echo "Usage: $0 arg1 arg2"
+    exit 1
 fi
